0f804d5b04fdf84abc59b8b1f1eeb2f52a65d7cd
Akshit Mandani <akshit.m@somaiya.edu>
2023-10-21 16:28:01 +0530

    DOM_Task_3_code

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function add_text() {
     const element = document.getElementById("btn-div");
     element.appendChild(text);
 }
+
+// DOM Maniputlation Task 3
+
+const header = document.getElementById("main-header");
+const add_item_text = document.getElementsByClassName("title")[0];
+
+header.style.borderBottom = "solid 3px #000";
+add_item_text.style.color = "green";
+add_item_text.style.fontWeight = "bold";
