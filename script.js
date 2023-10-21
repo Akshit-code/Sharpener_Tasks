2e43199160a8f596b48e7cdfbbb3ef96a274465e
Akshit Mandani <akshit.m@somaiya.edu>
2023-10-21 17:18:42 +0530

    Added DOM task 4 code

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ const add_item_text = document.getElementsByClassName("title")[0];
 header.style.borderBottom = "solid 3px #000";
 add_item_text.style.color = "green";
 add_item_text.style.fontWeight = "bold";
+
+//DOM Manipulation Task 4
+
+const item_list = document.getElementsByClassName("list-group-item");
+const itemCount = item_list.length;
+
+for (let i = 0; i < itemCount; i++) {
+    item_list[i].style.fontWeight = "bold";
+    item_list[i].style.color = "Orange"
+}
+
+item_list[2].style.backgroundColor = "green";
+
