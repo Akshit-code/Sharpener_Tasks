1c0603a54e95969b21b164876a5199162453f042
Akshit Mandani <akshit.m@somaiya.edu>
2023-11-03 21:32:55 +0530

    modified expense traker

--- Expense_tracker_app/script.js
+++ Expense_tracker_app/script.js
@@ -1,113 +1,78 @@
-const expenses = {
+const input = {
     amount: document.getElementById("amount"),
-    desc: document.getElementById("desc"),
-    category: document.getElementById("category"),
-};
+    desc : document.getElementById("desc"),
+    category: document.getElementById("category")
+}
 
-document.getElementById("submit").addEventListener("click", display_exp);
-document.addEventListener("DOMContentLoaded", loadExpenses);
+document.getElementById("btn_submit").addEventListener("click", function(e) {
+    e.preventDefault();
+    const expense = {
+        amount: input.amount.value,
+        desc: input.desc.value,
+        category: input.category.value,
+    }
+    console.log("Its working");
+    add_expenses(expense);
+    display_expense(expense);
+    console.log(expense);
+})
 
-function display_exp() {
-    const li = document.createElement("li");
-    const amount_text = document.createTextNode("Amount: " + expenses.amount.value);
-    const desc_text = document.createTextNode("Description: " + expenses.desc.value);
-    const category_text = document.createTextNode("Category: " + expenses.category.value);
-
+function display_expense(expense) {
+    let exp_unique_id = expense.unique_id;
+    const p = document.createElement("p");
+    const expense_text = `
+        Expense: ${expense.amount}, \t 
+        Description: ${expense.desc}, \t
+        Category: ${expense.category},
+    `
+    const expense_Text = document.createTextNode(expense_text);
+    p.appendChild(expense_Text);
+    const x = document.getElementById("display_expenses").appendChild(p);
+    
     const edit_btn = document.createElement("button");
+    const del_btn = document.createElement("button");
     edit_btn.innerHTML = "Edit";
-    edit_btn.addEventListener("click", function () {
-        edit_exp(li);
+    del_btn.innerHTML = "Delete";
+
+    p.appendChild(edit_btn);
+    p.appendChild(del_btn);
+
+    edit_btn.addEventListener("click", function() {
+        edit_expenses(expense);
+        p.remove();
+    } );
+    del_btn.addEventListener("click", function() {
+        delete_expenses(exp_unique_id);
+        p.remove();
     });
 
-    const delete_btn = document.createElement("button");
-    delete_btn.innerHTML = "Delete";
-    delete_btn.addEventListener("click", function () {
-        delete_exp(li);
-    });
+    input.amount.value = "";
+    input.desc.value = "";
+    input.category.value = "";
+}   
 
-    li.appendChild(amount_text);
-    li.appendChild(desc_text);
-    li.appendChild(category_text);
-    li.appendChild(edit_btn);
-    li.appendChild(delete_btn);
-
-    document.getElementById("display_list").appendChild(li);
-
-    saveExpense(expenses.amount.value, expenses.desc.value, expenses.category.value);
-
-    expenses.amount.value = "";
-    expenses.desc.value = "";
-    expenses.category.value = "";
+function add_expenses(expense) {
+    expense.unique_id = Date.now()*Math.random();
+    localStorage.setItem(expense.unique_id, JSON.stringify(expense));
 }
 
-function delete_exp(li) {
-    if (li) {
-        li.remove();
-        removeExpense(li);
+window.addEventListener("load", constUI());
+
+function constUI() {
+    for(let i=0; i<localStorage.length; i++) {
+        const unique_id = localStorage.key(i);
+        const expense = JSON.parse(localStorage.getItem(unique_id));
+        display_expense(expense);
     }
 }
 
-function edit_exp(li) {
-    if (li) {
-        const amountText = li.querySelector("li:nth-child(1)").textContent;
-        const descText = li.querySelector("li:nth-child(2)").textContent;
-      
-        const amountValue = parseFloat(amountText.substring(8));
-
-        // Populate the input fields with the extracted information
-        document.getElementById("amount").value = amountValue; // Set as a number or an empty string
-        document.getElementById("desc").value = descText.substring(12);
-
-        li.remove();
-        removeExpense(li);
-    }
+function edit_expenses(expense) {
+    input.amount.value = expense.amount;
+    input.desc.value = expense.desc;
+    input.category.value = expense.category;
+    delete_expenses(expense.unique_id);
 }
 
-function saveExpense(amount, desc, category) {
-    const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
-    expenses.push({ amount, desc, category });
-    localStorage.setItem('expenses', JSON.stringify(expenses));
-}
-
-function removeExpense(li) {
-    const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
-    const amountText = li.querySelector("li:nth-child(1)").textContent;
-    const descText = li.querySelector("li:nth-child(2)").textContent;
-    const categoryText = li.querySelector("li:nth-child(3)").textContent;
-    const expenseToRemove = { amount: amountText.substring(8), desc: descText.substring(12), category: categoryText.substring(9) };
-    const updatedExpenses = expenses.filter(expense => JSON.stringify(expense) !== JSON.stringify(expenseToRemove));
-    localStorage.setItem('expenses', JSON.stringify(updatedExpenses));
-}
-
-function loadExpenses() {
-    const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
-    const displayList = document.getElementById('display_list');
-    displayList.innerHTML = '';
-
-    expenses.forEach(expense => {
-        const li = document.createElement("li");
-        const amount_text = document.createTextNode("Amount: " + expense.amount);
-        const desc_text = document.createTextNode("Description: " + expense.desc);
-        const category_text = document.createTextNode("Category: " + expense.category);
-
-        const edit_btn = document.createElement("button");
-        edit_btn.innerHTML = "Edit";
-        edit_btn.addEventListener("click", function () {
-            edit_exp(li);
-        });
-
-        const delete_btn = document.createElement("button");
-        delete_btn.innerHTML = "Delete";
-        delete_btn.addEventListener("click", function () {
-            delete_exp(li);
-        });
-
-        li.appendChild(amount_text);
-        li.appendChild(desc_text);
-        li.appendChild(category_text);
-        li.appendChild(edit_btn);
-        li.appendChild(delete_btn);
-
-        displayList.appendChild(li);
-    });
+function delete_expenses(exp_unique_id) {
+    localStorage.removeItem(exp_unique_id);
 }
