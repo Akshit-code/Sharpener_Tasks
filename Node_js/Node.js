ef7bf7a6427d30856290ae61aba0e640e59885e5
Akshit Mandani <akshit.m@somaiya.edu>
2023-11-10 12:35:21 +0530

    Node url redirecting code

--- Node_js/Node.js
+++ Node_js/Node.js
@@ -2,6 +2,16 @@
 
 const http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Akshit Mandani');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    if (url === '/home') {
+      res.write('Welcome home');
+    } else if (url === '/about') {
+      res.write('Welcome to About Us page');
+    } else if (url === '/node') {
+      res.write('Welcome to my Node Js project');
+    } else {
+      res.write('Hello world !!');
+    }
+  
+    res.end();
 }).listen(8080);
