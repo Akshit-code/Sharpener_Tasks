7e1a83e4984dd55c22db6b823ca04d2a4e17f016
Akshit Mandani <akshit.m@somaiya.edu>
2023-11-10 11:18:39 +0530

    create node server task

--- Node_js/Node.js
+++ Node_js/Node.js
@@ -1,24 +1,7 @@
 // Task 1 Installation of Node Js
 
-// const http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('hello World!');
-// }).listen(8080);
-
-// Task 2 Lets Revise Javascript concepts
-
-let a = 10;
-let b = 5;
-let res = (x, y) => {return x * y };
-console.log(res(a,b));
-
-let student = {
-    first_name : "Akshit",
-    second_name : "Mandani",
-    greet() {
-        console.log("Hello " + this.first_name + " " +  this.second_name);
-    }
-}
-
-student.greet();
+const http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Akshit Mandani');
+}).listen(8080);
