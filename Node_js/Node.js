8dab1a0648b700c4b38ee373c145509d1b571b18
Akshit Mandani <akshit.m@somaiya.edu>
2023-11-10 17:55:51 +0530

    Node js read and write files

--- Node_js/Node.js
+++ Node_js/Node.js
@@ -1,17 +1,57 @@
-// Task 1 Installation of Node Js
-
 const http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    if (url === '/home') {
-      res.write('Welcome home');
-    } else if (url === '/about') {
-      res.write('Welcome to About Us page');
-    } else if (url === '/node') {
-      res.write('Welcome to my Node Js project');
-    } else {
-      res.write('Hello world !!');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    // Read the content from message.txt if it exists
+    let read_file = '';
+    try {
+      read_file = fs.readFileSync('message.txt', 'utf8');
+    } catch (error) {
+      console.log('File does not exist. Creating a new one.');
     }
-  
-    res.end();
-}).listen(8080);
+
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body>');
+    res.write(`<h1>${read_file}</h1>`);
+    res.write(`
+      <form action="/message" method="POST">
+        <input type="text" name="message">
+        <button type="submit">Send</button>
+      </form>
+    `);
+    res.write('</body></html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+
+      // Write to message.txt asynchronously
+      fs.writeFile('message.txt', message, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('File updated with new message');
+      });
+
+      // Redirect to the home page
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
+  }
+});
+
+server.listen(8080);
