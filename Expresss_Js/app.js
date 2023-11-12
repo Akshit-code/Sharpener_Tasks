7a120e6a529f3460a0970dcbd88b0ed4d6b4acd1
Akshit Mandani <akshit.m@somaiya.edu>
2023-11-12 06:01:54 +0530

    express task second done

--- Expresss_Js/app.js
+++ Expresss_Js/app.js
@@ -1,19 +1,25 @@
 const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); 
-});
+app.use(bodyParser.urlencoded( {extended : false}));
+app.use('/add-product', (req, res, next) => {
+    console.log('In Product page!');
+    res.send(`<form action = "/product" method="POST"> 
+        <input type = "text" name = "title">
+        <button type ="submit" >Submit </button> 
+    </form>`)
+    res.send('<h1>At the product page!</h1>');
+} );
 
-app.use((req, res, next) => {
+app.post("/product" , (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+});
+app.use("/", (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
-
 const server = http.createServer(app);
-
 server.listen(3000);
