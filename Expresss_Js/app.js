fc39bd3b89defbf356e29d9b51dcd3dd7d0baea7
Akshit Mandani <akshit.m@somaiya.edu>
2023-11-15 07:13:45 +0530

    router params

--- Expresss_Js/app.js
+++ Expresss_Js/app.js
@@ -1,6 +1,10 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -8,7 +12,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorContoller = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorContoller.getError404);
+app.use(errorController.get404);
 
 app.listen(3000);
--- Expresss_Js/models/product.js
+++ Expresss_Js/models/product.js
@@ -1,35 +1,48 @@
-const { json } = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const getProductFromFile = cb => {
-    const p = path.join(path.dirname(process.mainModule.filename,
-        'data',
-        'products.json'));
-    fs.readFile(p, (err, fileContent) => {
-        if(err) {
-            cb([]);
-        }
-        cb(JSON.parse(fileContent)); 
-    });
-}
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        getProductFromFile();
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err)=> console.log(err) );
-        });
-    }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb) {
-        getProductFromFile(cb);
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    })
+  }
+};
